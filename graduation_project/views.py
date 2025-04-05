cf1e0b27e2a1fe45b3312c7891c55bc9ee04a17e
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2025-04-05 19:30:58 +0200

    update in frontend responsive

--- graduation_project/views.py
+++ graduation_project/views.py
@@ -60,7 +60,7 @@ def ProjectList(request):
 
    # Pagination
     page = request.GET.get('page', 1)  # Get the current page number from the request
-    paginator = Paginator(projects, 15)  # Show 10 projects per page
+    paginator = Paginator(projects, 10)  # Show 10 projects per page
     try:
         paginated_projects = paginator.page(page)
     except PageNotAnInteger:
